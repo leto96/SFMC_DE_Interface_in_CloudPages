0ca5f5a45298b5efafb5423acf02d6535a9fdf3f
leto96 <matheusleto08@gmail.com>
2021-06-12 15:01:38 -0300

    Retrieve DE by name (like)

--- Backend.js
+++ Backend.js
@@ -24,13 +24,33 @@
           break;
       }
     } else { // Data Extension
-      
+      switch( operationType ){
+        case 'retrieveWithName':
+          result = retrieveDEByNameLike(api);
+          sendResponse( reduceDEs(result) );
+        default:
+          Platform.Function.RaiseError('INVALID_OPERATION_ON_FOLDER');
+          break;
+      }
     }
     
   } catch (error) {
     Write('error\n');
     Write(Stringify(error.message));
 
+  }
+
+  // --------------- MAIN -------------------
+  function retrieveDEByNameLike(api){
+    var postedData = getPostData();
+    var DENameToLookLike = postedData.name;
+    
+    if( DENameToLookLike == null || DENameToLookLike == undefined || DENameToLookLike.length < 4 ){
+      Platform.Function.RaiseError('INVALID_SEARCH_NAME');
+    }
+
+    var DEsFound = DataExtensionCollection({api:api}).getDEWithNameLike(DENameToLookLike);
+    return DEsFound;
   }
 
 
@@ -66,6 +86,17 @@
         id: folder.ID,
         parentID: folder.ParentFolder.ID,
         dateCreated: folder.CreatedDate
+      };
+    });
+  }
+
+  function reduceDEs(DEs){
+    return mapArray(DEs, function(de) {
+      return {
+        name: de.Name,
+        customerKey: de.CustomerKey,
+        folderID: de.CategoryID,
+        dateCreated: de.CreatedDate
       };
     });
   }
@@ -295,7 +326,116 @@
     }
   }
 
+  // Aux to use Array.map
   function mapArray(array, callback, thisArg) {
     var T, A, k;var O = array;var len = O.length;if (typeof callback !== 'function') {throw new TypeError(callback + ' is not a function');}if (arguments.length > 2) {T = thisArg;}A = [];k = 0;while (k < len) {var kValue, mappedValue;kValue = O[k];mappedValue = callback.call(T, O[k], k, O);A.push(mappedValue);k++;}return A;
+  }
+
+  // Data Extension Finder
+  function DataExtensionCollection(configuration) {
+    if(!configuration) Platform.Function.RaiseError('An configuration Object is required');
+    if(!configuration.api) Platform.Function.RaiseError('Api attribute is required');
+    var api = configuration.api;
+  
+    // if(!customerKey && !name) Platform.Function.RaiseError('Either \"customerKey\" or \"name\" is required');
+  
+    function generateDataExtensionObject(obj){
+      if(!obj) return;
+      var formatedObject = {
+        ObjectID: obj.ObjectID,
+        PartnerKey: obj.PartnerKey,
+        CustomerKey: obj.CustomerKey,
+        Name: obj.Name,
+        CreatedDate: obj.CreatedDate,
+        ModifiedDate: obj.ModifiedDate,
+        Description: obj.Description,
+        IsSendable: obj.IsSendable,
+        IsTestable: obj.IsTestable,
+        CategoryID: obj.CategoryID,
+        Status: obj.Status,
+        IsPlatformObject: obj.IsPlatformObject,
+        DataRetentionPeriodLength: obj.DataRetentionPeriodLength,
+        DataRetentionPeriodUnitOfMeasure: obj.DataRetentionPeriodUnitOfMeasure,
+        RowBasedRetention: obj.RowBasedRetention,
+        ResetRetentionPeriodOnImport: obj.ResetRetentionPeriodOnImport,
+        DeleteAtEndOfRetentionPeriod: obj.DeleteAtEndOfRetentionPeriod,
+        RetainUntil: obj.RetainUntil,
+        DataRetentionPeriod: obj.DataRetentionPeriod
+      }
+      if(obj.Template && obj.Template.CustomerKey){
+        formatedObject.Template = {CustomerKey: obj.Template.CustomerKey};
+      }else{
+        formatedObject.Template = {CustomerKey: null};
+      }
+      if(obj.SendableSubscriberField && obj.SendableSubscriberField.Name){
+        formatedObject.SendableSubscriberField = {Name: obj.SendableSubscriberField.Name};
+      }else{
+        formatedObject.SendableSubscriberField = {Name: null};
+      }
+      if(obj.SendableDataExtensionField && obj.SendableDataExtensionField.Name){
+        formatedObject.SendableDataExtensionField = {Name: obj.SendableDataExtensionField.Name};
+      }else{
+        formatedObject.SendableDataExtensionField = {Name: null};
+      }
+      if(obj.Client && obj.Client.ID){
+        formatedObject.Client = {ID: obj.Client.ID};
+      }else{
+        formatedObject.Client = {ID: null};
+      }
+  
+      return formatedObject;
+    }
+  
+    function retriveDEs(prop, operator, value){
+      var retrieveFilter = {
+        Property: prop,
+        SimpleOperator: operator,
+        Value: value
+      };
+  
+      var retrievedDE = api.retrieve('DataExtension', [
+        'ObjectID',
+        'PartnerKey',
+        'CustomerKey',
+        'Name',
+        'CreatedDate',
+        'ModifiedDate',
+        'Description',
+        'IsSendable',
+        'IsTestable',
+        'CategoryID',
+        'Status',
+        'IsPlatformObject',
+        'DataRetentionPeriodLength',
+        'DataRetentionPeriodUnitOfMeasure',
+        'RowBasedRetention',
+        'ResetRetentionPeriodOnImport',
+        'DeleteAtEndOfRetentionPeriod',
+        'RetainUntil',
+        'DataRetentionPeriod',
+        'Template.CustomerKey',
+        'SendableSubscriberField.Name',
+        'SendableDataExtensionField.Name',
+        'Client.ID'
+        ], retrieveFilter
+      );
+      return retrievedDE.Results;
+    }
+  
+    function getDEWithNameLike(name){
+      var des = retriveDEs('Name', 'like', name);
+  
+      return mapArray(des, function(de){
+        return generateDataExtensionObject(de);
+      });
+    }
+  
+    function mapArray(array, callback, thisArg) {
+      var T, A, k;var O = array;var len = O.length;if (typeof callback !== 'function') {throw new TypeError(callback + ' is not a function');}if (arguments.length > 2) {T = thisArg;}A = [];k = 0;while (k < len) {var kValue, mappedValue;kValue = O[k];mappedValue = callback.call(T, O[k], k, O);A.push(mappedValue);k++;}return A;
+    }
+    
+    return {
+      getDEWithNameLike: getDEWithNameLike
+    }
   }
 // </script>
